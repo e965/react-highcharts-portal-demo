896fcbaf5a7a5b70a36c1b03caf7975a8851df17
Dmitry <dmitry@cojam.ru>
2021-03-25 22:31:43 +0500

    initial commin

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+/** @jsxRuntime classic */
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
